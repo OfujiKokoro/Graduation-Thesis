f4f02fae4a640f5a4e59f9b8814e8ff019b31b4d
Kokoro <cocoron1412ah5794@gmail.com>
2025-06-12 16:27:09 +0900

    Add script

---
+++ data/Atlantic/download_Atlantic.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Set the directory to save downloaded files
+DOWNLOAD_DIR="sra"
+FASTQ_DIR="fastq"
+BGZIP_DIR="fastqgz"
+
+# Create directories if they do not exist
+mkdir -p $DOWNLOAD_DIR $FASTQ_DIR $BGZIP_DIR
+
+# List of SRR files to download (example SRR IDs)
+SRR_LIST="SRR_Acc_List_Atlantic.txt"
+
+# Download SRR files from NCBI using SRA Toolkit and save them in DOWNLOAD_DIR
+while read -r SRR; do
+    echo "Downloading $SRR..."
+    prefetch --max-size 80G "$SRR" -O "$DOWNLOAD_DIR"
+done < "$SRR_LIST"
+
+echo "Download completed."
+
+# Convert downloaded SRR files to fastq and save them in FASTQ_DIR
+for sra_file in $DOWNLOAD_DIR/*/*.sra; do
+    echo "Converting $sra_file to fastq..."
+    fastq-dump $sra_file --split-files -O $FASTQ_DIR/
+done
+
+echo "Fastq conversion completed."
+
+
+# Convert fastq files to bgzip and save them in BGZIP_DIR
+for fastq_file in $FASTQ_DIR/*.fastq; do
+    filename=$(basename -- "$fastq_file")
+    filename_without_extension="${filename%.*}"
+    bgzip_file="$BGZIP_DIR/$filename_without_extension.fastq.gz"
+    echo "Converting $fastq_file to $bgzip_file"
+    bgzip -c $fastq_file > $bgzip_file
+done
+
+echo "Conversion to bgzip completed."
